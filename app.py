0a1eb85ed50b4d50ac2b66ecf83017dadad78298
gnyirjesy <gnyirjesy@users.noreply.github.com>
2020-08-25 21:43:51 -0400

    Updated data file path

--- app.py
+++ app.py
@@ -67,9 +67,9 @@ app = dash.Dash(__name__)
 app.title = 'Facebook Ad Campaign Analysis'
 
 #Define location of file and read in data
-#url = 'https://raw.githubusercontent.com/gnyirjesy/Ad-Campaign-Dashboard/master/Data/ads_clean.csv'
-#ads = pd.read_csv(url, index_col=0)
-ads = pd.read_csv('/Users/gabbynyirjesy/Desktop/GitHub/Facebook_Ad_Campaign_Dashboard/Ad-Campaign-Dashboard/Data/ads_clean.csv')
+url = 'https://raw.githubusercontent.com/gnyirjesy/Ad-Campaign-Dashboard/master/Data/ads_clean.csv'
+ads = pd.read_csv(url, index_col=0)
+#ads = pd.read_csv('/Users/gabbynyirjesy/Desktop/GitHub/Facebook_Ad_Campaign_Dashboard/Ad-Campaign-Dashboard/Data/ads_clean.csv')
 
 
 #Create the app layout
